f0fff902de02aabc2b38481c1e4151c7d684dcb6
Nethmi Thathsarani <thathsaraninethmi942@gmail.com>
2024-01-22 22:49:59 +0530

    update backend project

--- POS_BackEnd/src/main/java/lk/ijse/gdse66/POS_BackEnd/servlet/CustomerServlet.java
+++ POS_BackEnd/src/main/java/lk/ijse/gdse66/POS_BackEnd/servlet/CustomerServlet.java
@@ -1,6 +1,12 @@
 package lk.ijse.gdse66.POS_BackEnd.servlet;
 
+import lk.ijse.gdse66.POS_BackEnd.bo.BOFactory;
+import lk.ijse.gdse66.POS_BackEnd.bo.custom.CustomerBO;
+import lk.ijse.gdse66.POS_BackEnd.dto.CustomerDTO;
+import sun.jvm.hotspot.debugger.DataSource;
+
 import java.io.*;
+import java.sql.Connection;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -9,6 +15,12 @@ import javax.servlet.annotation.*;
 public class CustomerServlet extends HttpServlet {
     private String message;
 
+
+    DataSource dataSource;
+
+    private final CustomerBO customerBO = (CustomerBO) BOFactory.getBoFactory().getBO(BOFactory.BOTypes.CUSTOMER);
+
+
     public void init() {
         message = "Hello World!";
     }
@@ -16,8 +28,32 @@ public class CustomerServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        PrintWriter writer = resp.getWriter();
+        resp.setContentType("application/json");
+
+        Connection connection = null;
+
         resp.addHeader("Access-Control-Allow-Origin", "*");
-    }
+
+//        try {
+//            connection = dataSource.getConnection();
+//
+//            CustomerDTO customerDTO = new CustomerDTO(
+//                    req.getParameter("cusId"),
+//                    req.getParameter("cusName"),
+//                    req.getParameter("cusAddress"),
+//                    req.getParameter("cusTp")
+//            );
+//
+//            try {
+//                if (customerBO.addCustomer(connection, customerDTO)){
+//
+//                }
+//            }
+//    }
+
+}
 
     public void destroy() {
     }
