c31e75d94332e8b8ec25436bd5c95108c9ca8272
Nethmi Thathsarani <thathsaraninethmi942@gmail.com>
2024-01-30 10:16:20 +0530

    add

--- POS_BackEnd/src/main/java/lk/ijse/gdse66/POS_BackEnd/servlet/CustomerServlet.java
+++ POS_BackEnd/src/main/java/lk/ijse/gdse66/POS_BackEnd/servlet/CustomerServlet.java
@@ -1,5 +1,7 @@
 package lk.ijse.gdse66.POS_BackEnd.servlet;
 
+import jakarta.json.Json;
+import jakarta.json.JsonObjectBuilder;
 import lk.ijse.gdse66.POS_BackEnd.bo.BOFactory;
 import lk.ijse.gdse66.POS_BackEnd.bo.custom.CustomerBO;
 import lk.ijse.gdse66.POS_BackEnd.dto.CustomerDTO;
@@ -39,12 +41,48 @@ public class CustomerServlet extends HttpServlet {
 
         resp.addHeader("Access-Control-Allow-Origin", "*");
 
-       try {
-           connection = dataSource.getConnection();
-       } catch (SQLException e) {
-           e.printStackTrace();
-       }
+        try {
+            connection = dataSource.getConnection();
 
+            CustomerDTO customerDTO = new CustomerDTO(
+                    req.getParameter("cusId"),
+                    req.getParameter("cusName"),
+                    req.getParameter("cusAddress"),
+                    req.getParameter("cusTp")
+            );
+
+            try {
+                if (customerBO.addCustomer(connection, customerDTO)) {
+                    JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+                    resp.setStatus(HttpServletResponse.SC_ACCEPTED);
+                    objectBuilder.add("status", 200);
+                    objectBuilder.add("message", "Successfully Added");
+                    objectBuilder.add("data", "");
+                    writer.print(objectBuilder.build());
+                }
+            } catch (ClassNotFoundException e) {
+                JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+                objectBuilder.add("status",400);
+                objectBuilder.add("message","Error");
+                objectBuilder.add("data", e.getLocalizedMessage());
+                writer.print(objectBuilder.build());
+                resp.setStatus(HttpServletResponse.SC_OK);
+                e.printStackTrace();
+                e.printStackTrace();
+            }
+
+            connection.close();
+
+
+        }catch (SQLException e){
+            JsonObjectBuilder objectBuilder  = Json.createObjectBuilder();
+            objectBuilder.add("status" , 400);
+            objectBuilder.add("message", "Error");
+            objectBuilder.add("data" , e.getLocalizedMessage());
+            writer.print(objectBuilder.build());
+            resp.setStatus(HttpServletResponse.SC_OK);
+            e.printStackTrace();
+        }
 
     }
 
